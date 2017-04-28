860f55cfcfa98bfbc47de6c6bfb2d49e9859410e
achujelas123 <achujel3@gmail.com>
2017-04-28 17:51:49 +0300

    testing query and path params

--- src/main/java/org/advjaxrs/rest/MyResource.java
+++ src/main/java/org/advjaxrs/rest/MyResource.java
@@ -1,21 +1,28 @@
 package org.advjaxrs.rest;
 
-import javax.inject.Singleton;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
-@Path("test")
-@Singleton
+@Path("{pathParam}/test")
 public class MyResource {
 
     private int count;
+    @PathParam("pathParam")
+    private String pathParamExample;
+    @QueryParam("queryParam")
+    private String queryParamExample;
+
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String testMethod() {
-        return "The count is: " + count++;
+        return "The count is: " + count++
+                + "\nPath param: " + pathParamExample
+                + "\nQuery param: " + queryParamExample;
     }
 
 }
