83eb88077ebbdce61e2002e613ce67577fe893b9
achujelas123 <achujel3@gmail.com>
2017-04-28 17:29:38 +0300

    first commit

---
+++ src/main/java/org/advjaxrs/rest/MyApp.java
@@ -0,0 +1,10 @@
+package org.advjaxrs.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("webapi")
+public class MyApp extends Application {
+
+
+}
